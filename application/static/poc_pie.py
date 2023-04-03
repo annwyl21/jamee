ebb5e3d027e1ee49eb80e4c117c612bce18f15df
Ellen Houghton <ellen.a.ash@gmail.com>
2023-04-03 15:10:45 +0100

    updated graph code with ONS example figures

--- application/static/poc_pie.py
+++ application/static/poc_pie.py
@@ -3,8 +3,8 @@ from matplotlib import pyplot as plt
 
 # creates a dataframe that matplot can use
 df = pd.DataFrame({
-    'expenditure': ['rent', 'council tax', 'bills', 'groceries', 'other'],
-    'spending': [500, 80, 100, 50, 10]
+    'expenditure': ['housing', 'food and drink', 'energy bills', 'petrol or diesel', 'train fares', 'bus fares', 'eating and drinking', 'holidays', 'clothes and footwear'],
+    'spending': [368, 771, 112, 98, 18, 26, 285, 128, 181]
 })
 
 # creates the size of the pie chart
@@ -13,8 +13,9 @@ plt.figure(figsize=(6,4))
 plt.subplot()
 # which data to use and to display a whole figure percentage
 plt.pie(df['spending'], autopct='%d%%')
-plt.title("Pie Chart to show spending distribution")
-plt.legend(df['expenditure'], loc='upper right')
+plt.axis('equal')
+plt.title("Spending distribution for Average UK Household - Renting")
+plt.legend(df['expenditure'], loc='upper right', bbox_to_anchor=(1,1), fontsize=7)
 
 # put that pie chart in a saved file
 plt.savefig('application/static/piechart.png')
--- application/static/poc_stack.py
+++ application/static/poc_stack.py
@@ -1,39 +1,51 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-expenditure = ['My Spending', 'Average Spending']
-rent = [500, 200]
-bills = [80, 100]
-council_tax = [100, 80]
-groceries = [50, 80]
-other = [80, 350]
+expenditure = ['My Spending', 'UK Average']
+housing = [981, 1054]
+groceries = [372, 368]
+bills = [107, 112]
+fuel = [102, 98]
+train = [15, 18]
+bus = [35, 26]
+cafe = [382, 128]
+holiday = [115, 128]
+clothes = [161, 181]
 
 # number of columns in the stacked bar chart
 x = range(2)
 
 # calculations to add up the height of the stacks
-c_bottom = np.add(rent, bills)
-g_bottom = np.add(c_bottom, council_tax)
-o_bottom = np.add(g_bottom, groceries)
+bills_bottom = np.add(housing, groceries)
+fuel_bottom = np.add(bills_bottom, bills) # each time taking the height of the existing stack and adding on the previous stacked bar
+train_bottom = np.add(fuel_bottom, fuel)
+bus_bottom = np.add(train_bottom, train)
+cafe_bottom = np.add(bus_bottom, bus)
+holiday_bottom = np.add(cafe_bottom, cafe)
+clothes_bottom = np.add(holiday_bottom, holiday)
 
 # code to create the graph
 plt.figure(figsize=(10, 8))
 ax = plt.subplot()
-plt.bar(x, rent)
-plt.bar(x, bills, bottom = rent)
-plt.bar(x, council_tax, bottom = c_bottom)
-plt.bar(x, groceries, bottom = g_bottom) # for 2 columns, display the value in the groceries list, use the start position for the colour as the sum of all the values of the heights of the other bars
-plt.bar(x, other, bottom = o_bottom)
+plt.bar(x, housing)
+plt.bar(x, groceries, bottom = housing)
+plt.bar(x, bills, bottom = bills_bottom)
+plt.bar(x, fuel, bottom = fuel_bottom) # for 2 columns, display the cost of fuel, use the start position for the stacked bar as the sum of all the values of the heights of the other bars
+plt.bar(x, train, bottom = train_bottom)
+plt.bar(x, bus, bottom = bus_bottom)
+plt.bar(x, cafe, bottom = cafe_bottom)
+plt.bar(x, holiday, bottom = holiday_bottom)
+plt.bar(x, clothes, bottom = clothes_bottom)
 
 # just have 2 markers on the x-axis because the length of the expenditure list is 2
 ax.set_xticks(range(len(expenditure)))
 # use the strings in the expenditure list to label those markers
 ax.set_xticklabels((expenditure))
 
-plt.title("Stacked bar chart showing a comparison of spending distribution")
+plt.title("Stacked bar chart: Comparison of Spending Distribution for Mortgage Paying Households")
 plt.xlabel("Comparison")
-plt.ylabel("Spending Type £")
-plt.legend(("Rent", "Bills", "Council Tax", "Groceries", "Other"))
+plt.ylabel("Spending Type in £'s")
+plt.legend(("Housing", "Food and Drink", "Energy Bills", "Fuel", "Train Fares", "Bus Fares", "Eating and Drinking", "Holidays", "Clothes and Footwear"))
 
 plt.savefig('application/static/barstack.png')
 
