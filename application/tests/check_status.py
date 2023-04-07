cb087367a9994e56f98a5560043b9edb4b9ca5d5
Ellen Houghton <ellen.a.ash@gmail.com>
2023-04-07 14:46:14 +0100

    created branch to look at pytest

---
+++ application/tests/check_status.py
@@ -0,0 +1,4 @@
+import pytest
+
+# documentation link - I don't understand everything but I am going to give it a try
+# Here I am trying to write a test to check the status code of our pages on loading - looking to see if all the links work
