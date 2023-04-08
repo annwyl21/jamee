fb4c9df692d9a932e7159c5aebdd24680025e994
Ellen Houghton <ellen.a.ash@gmail.com>
2023-04-08 15:47:45 +0100

    add 2 additional methods to database class

--- application/static/scripts/benefits.sql
+++ application/static/scripts/benefits.sql
@@ -1,48 +1,264 @@
--- Using a database as a content management system
--- url endpoints are in the name field and attach to this base url https://www.gov.uk/
+Create Database benefits;
 
--- CREATE DATABASE budget_management;
-use budget_management;
--- drop database budget_management;
+Use benefits;
 
-create table benefits
-	(benefit_id int not null auto_increment primary key,
-    benefit_name varchar (50),
-    how varchar (500),
-    what varchar (500));
-    
--- Child Benefit data
-insert into benefits(benefit_name, how, what) 
-values 
-    ('child-benefit', 
-    'You get Child Benefit if you are responsible for bringing up a child who is; under 16 or under 20 if they stay in approved education or training. Only one person can get Child Benefit for a child. There is no limit to how many children you can claim for.', 
-    'There are 2 child benefit rates. £24. per week for the eldest child or an only child, and £15.90 for each additional child, paid every 4-weeks.');
+drop database benefits;
 
--- Housing Benefit data
-insert into benefits(benefit_name, how, what) 
-values 
-    ('housing-benefit', 
-    'Housing Benefit can help you pay your rent if you are unemployed, on a low income or claiming benefits. It is being replaced by Universal Credit. You can only make a new claim for Housing Benefit if either of the following apply; you have reached State Pension age or you are in supported, sheltered or temporary housing.', 
-    'You may get help with all or part of your rent. There is no set amount of Housing Benefit and what you get will depend on whether you rent privately or from a council.');
+CREATE TABLE budget_user
+(user_id int not null auto_increment primary key,
+username varchar(50),
+user_password varchar(50),
+title varchar(10),
+first_name varchar(50),
+last_name varchar(50)
+);
 
--- ESA Benefit data
-insert into benefits(benefit_name, how, what) 
-values 
-    ('employment-support-allowance', 
-    'You can apply for Employment and Support Allowance (ESA) if you have a disability or health condition that affects how much you can work. ESA gives you; money to help with living costs if you are unable to work or support to get back into work if you are able to. You can apply if you are employed, self-employed or unemployed.', 
-    'How much you get will depend on what stage your application is at, as well as things like your age and whether you are able to get back into work. If you get new style ESA you will earn Class 1 National Insurance credits, which can help towards your State Pension and some benefits in the future.');
 
--- JSA Benefit data
-insert into benefits(benefit_name, how, what) 
-values 
-    ('jobseekers-allowance', 
-    'You can apply for New Style Jobseekers Allowance (JSA) to help you when you are looking for work. You cannot apply for income-based JSA any more. If you are currently getting income-based JSA, you will keep getting payments while you are eligible until your claim ends.', 
-    'When you apply to claim JSA, your work coach will make an agreement with you to look for work. This agreement is called a Claimant Commitment. Your Claimant Commitment could include; what you need to do to look for work. You can search and apply for work using the Find a job service.');
+CREATE TABLE budget_account
+(account_id int not null auto_increment primary key,
+account_name varchar(50),
+account_type varchar(50),
+income_total decimal
+);
+ALTER TABLE budget_account
+ADD COLUMN user_id int,
+ADD foreign key(user_id) references budget_user(user_id);
 
--- Universal Credit
-insert into benefits(benefit_name, how, what) 
-values 
-    ('universal-credit', 
-    'Universal Credit is a payment to help with your living costs. It is paid monthly - or twice a month for some people in Scotland. You may be able to get it if you are on a low income, out of work or you cannot work.', 
-    'How much Universal Credit you get depends on; your standard allowance any extra amounts that apply to you and any money taken off your payment. Universal Credit is paid monthly and is based on your circumstances each month. This is called your assessment period and it starts the day you make your claim.');
+Create table employment_support_allowance
+(esa_id int not null auto_increment primary key,
+firstname varchar(50),
+lastname varchar(50),
+age INT,
+weekly_esa decimal ,
+monthly_esa decimal,
+annual_esa decimal)
+;
 
+Create table job_seeker_allowance
+(jsa_id int not null auto_increment primary key,
+firstname varchar(50),
+lastname varchar(50),
+age INT,
+weekly_jsa decimal ,
+monthly_jsa decimal,
+annual_jsa decimal)
+;
+
+Create table child_tax_credit
+(cta_id int not null auto_increment primary key,
+firstname varchar(50),
+lastname varchar(50),
+age INT,
+weekly_eta decimal ,
+monthly_eta decimal,
+annual_eta decimal)
+;
+
+Create table housing_benefit
+(hb_id int not null auto_increment primary key,
+firstname varchar(50),
+lastname varchar(50),
+age INT,
+weekly_hb decimal ,
+monthly_hb decimal,
+annual_hb decimal)
+;
+
+CREATE TABLE budget_user
+(user_id int not null auto_increment primary key,
+username varchar(50),
+user_password varchar(50),
+title varchar(10),
+first_name varchar(50),
+last_name varchar(50)
+);
+
+ALTER TABLE employment_support_allowance
+ADD COLUMN user_id int,
+ADD foreign key(user_id) references budget_user(user_id);
+
+ALTER TABLE job_seeker_allowance
+ADD COLUMN user_id int,
+ADD foreign key(user_id) references budget_user(user_id);
+
+ALTER TABLE child_tax_credit
+ADD COLUMN user_id int,
+ADD foreign key(user_id) references budget_user(user_id);
+
+
+
+CREATE TABLE income
+(income_id int not null auto_increment primary key,
+income_source varchar(50),
+income_date varchar(50),
+income_total decimal
+);
+
+ALTER TABLE employment_support_allowance
+ADD COLUMN income_id int,
+ADD foreign key(income_id) references income(income_id);
+
+ALTER TABLE job_seeker_allowance
+ADD COLUMN income_id int,
+ADD foreign key(income_id) references income(income_id);
+
+ALTER TABLE child_tax_credit
+ADD COLUMN income_id int,
+ADD foreign key(income_id) references income(income_id);
+
+ALTER TABLE housing_benefit
+ADD COLUMN income_id int,
+ADD foreign key(income_id) references income(income_id);
+
+
+
+
+CREATE TABLE expense
+(expense_id int not null auto_increment primary key,
+expense_source varchar(50),
+expense_date varchar(50),
+expense_total decimal
+);
+
+ALTER TABLE employment_support_allowance
+ADD COLUMN expense_id int,
+ADD foreign key(expense_id) references expense(expense_id);
+
+ALTER TABLE job_seeker_allowance
+ADD COLUMN expense_id int,
+ADD foreign key(expense_id) references expense(expense_id);
+
+ALTER TABLE child_tax_credit
+ADD COLUMN expense_id int,
+ADD foreign key(expense_id) references expense(expense_id);
+
+
+
+
+ALTER TABLE employment_support_allowance
+ADD COLUMN user_id int,
+ADD foreign key(user_id) references budget_user(user_id);
+
+ALTER TABLE job_seeker_allowance
+ADD COLUMN user_id int,
+ADD foreign key(user_id) references budget_user(user_id);
+
+ALTER TABLE child_tax_credit
+ADD COLUMN user_id int,
+ADD foreign key(user_id) references budget_user(user_id);
+
+ALTER TABLE housing_benefit
+ADD COLUMN user_id int,
+ADD foreign key(user_id) references budget_user(user_id);
+
+
+-- CREATE TABLE category
+-- -- To look at types of incoming and outgoing money e.g. food, holidays, bills etc 
+-- (category_id int not null auto_increment primary key,
+-- category_name varchar(50)
+-- );
+-- Adding foreign keys to 'category' table 
+-- ALTER TABLE category
+-- ADD COLUMN user_id int,
+-- ADD foreign key(user_id) references budget_user(user_id);
+
+-- ALTER TABLE category
+-- ADD COLUMN income_id int,
+-- ADD foreign key(income_id) references income(income_id);
+
+-- ALTER TABLE category
+-- ADD COLUMN expense_id int,
+-- ADD foreign key(expense_id) references expense(expense_id);
+
+-- CREATE TABLE weekly_budget
+-- (weekly_budget_id int not null auto_increment primary key,
+-- weekly_balance decimal
+-- );
+
+-- Adding foreign keys to 'weekly_budget' table 
+-- ALTER TABLE weekly_budget
+-- ADD COLUMN user_id int,
+-- ADD foreign key(user_id) references budget_user(user_id);
+
+-- ALTER TABLE weekly_budget
+-- ADD COLUMN income_id int,
+-- ADD foreign key(income_id) references income(income_id);
+
+-- ALTER TABLE weekly_budget
+-- ADD COLUMN expense_id int,
+-- ADD foreign key(expense_id) references expense(expense_id);
+
+-- ALTER TABLE weekly_budget
+-- ADD COLUMN category_id int,
+-- ADD foreign key(category_id) references category(category_id);
+
+
+
+
+-- CREATE TABLE monthly_budget
+-- (monthly_budget_id int not null auto_increment primary key,
+-- monthly_balance decimal
+-- );
+
+-- -- Adding foreign keys to 'monthly_budget' table 
+-- ALTER TABLE monthly_budget
+-- ADD COLUMN user_id int,
+-- ADD foreign key(user_id) references budget_user(user_id);
+
+-- ALTER TABLE monthly_budget
+-- ADD COLUMN income_id int,
+-- ADD foreign key(income_id) references income(income_id);
+
+-- ALTER TABLE monthly_budget
+-- ADD COLUMN expense_id int,
+-- ADD foreign key(expense_id) references expense(expense_id);
+
+-- ALTER TABLE monthly_budget
+-- ADD COLUMN category_id int,
+-- ADD foreign key(category_id) references category(category_id);
+
+
+-- CREATE TABLE annual_budget
+-- (annual_budget_id int not null auto_increment primary key,
+-- annual_balance decimal
+-- );
+
+-- -- Adding foreign keys to 'weekly_budget' table 
+-- ALTER TABLE annual_budget
+-- ADD COLUMN user_id int,
+-- ADD foreign key(user_id) references budget_user(user_id);
+
+-- ALTER TABLE annual_budget
+-- ADD COLUMN income_id int,
+-- ADD foreign key(income_id) references income(income_id);
+
+-- ALTER TABLE annual_budget
+-- ADD COLUMN expense_id int,
+-- ADD foreign key(expense_id) references expense(expense_id);
+
+-- ALTER TABLE annual_budget
+-- ADD COLUMN category_id int,
+-- ADD foreign key(category_id) references category(category_id);
+
+
+-- CREATE TABLE custom_budget
+-- (custom_budget_id int not null auto_increment primary key,
+-- custom_balance decimal
+-- );
+
+-- -- Adding foreign keys to 'custom_budget' table 
+-- ALTER TABLE custom_budget
+-- ADD COLUMN user_id int,
+-- ADD foreign key(user_id) references budget_user(user_id);
+
+-- ALTER TABLE custom_budget
+-- ADD COLUMN income_id int,
+-- ADD foreign key(income_id) references income(income_id);
+
+-- ALTER TABLE custom_budget
+-- ADD COLUMN expense_id int,
+-- ADD foreign key(expense_id) references expense(expense_id);
+
+-- ALTER TABLE custom_budget
+-- ADD COLUMN category_id int,
+-- ADD foreign key(category_id) references category(category_id);
