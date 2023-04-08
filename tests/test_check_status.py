9222c2727b3fa0072c4526708334b468c4a2198e
Ellen Houghton <ellen.a.ash@gmail.com>
2023-04-08 16:22:08 +0100

    add page tests

--- tests/test_check_status.py
+++ tests/test_check_status.py
@@ -1,20 +1,32 @@
 import pytest
 from application import app
 
+# type this in your terminal to see the tests passing
+# python -m pytest
+
 # documentation link - I don't understand everything but I am going to give it a try
 # https://flask.palletsprojects.com/en/2.2.x/testing/
-# Here I am trying to write a test to check the status code of our pages on loading - looking to see if all the links work
 
+# I think this starts the app in the background in a 'testing' mode
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     return app.test_client()
 
+# This is pytest looking for some text in each page when the page has loaded.
+# index page - an information page
 def test_request_index(client):
     response = client.get("/")
     assert b"<h1>Welcome to ChipIn, the best personal finance tool in the UK.</h1>" in response.data
 
+# dashboard page - a page with elements that generate using database queries that might fail
 def test_request_dashboard(client):
     response = client.get("/dashboard")
     assert b"<h1>Dashboard</h1>" in response.data
+
+# articles/ benefits template page - a page where the text changes depending on which route was selected
+# and different text is selected from the database for each page
+def test_request_benefits(client):
+    response = client.get("/child-benefit")
+    assert b"You get Child Benefit if you are responsible for bringing up a child" in response.data
 
