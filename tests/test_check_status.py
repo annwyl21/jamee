308bcdc85bfc0037a22404ade93eb5a882e6778d
Ellen Houghton <ellen.a.ash@gmail.com>
2023-04-10 12:19:02 +0100

    created pytests and BDD for dash pw, pcm, pa calculators

--- tests/test_check_status.py
+++ tests/test_check_status.py
@@ -14,29 +14,69 @@ def client():
     app.config['TESTING'] = True
     return app.test_client()
 
-# This is pytest looking for some text in each page when the page has loaded.
-# index page - an information page
+# BDD: GIVEN, WHEN, THEN
+
+# TESTING THE HOME PAGE
+# GIVEN I have entered localhost:5000/ to request the home page
 def test_request_index(client):
+# WHEN I 'GET' the page
     response = client.get("/")
+# THEN the following text should load at the end of the page
     assert b"<h3>Why ChipIn?</h3>" in response.data
 
-# dashboard page - a page with elements that generate using database queries that might fail
+# TESTING THE DASHBOARD PAGE - a page with elements that generate using database queries that might fail
+# GIVEN I have entered my monthly spending on the form page
 def test_request_dashboard(client):
+# WHEN I click submit
     response = client.get("/dashboard")
+# THEN the dashboard page should load and I should see 2 graphs and 3 tables
+# This test is checking to see that the last of those 5 elements has loaded
     assert b"<h2>Your annual spending</h2>" in response.data
 
-# articles/ benefits template page - a page where the text changes depending on which route was selected
-# and different text is selected from the database for each page
+
+
+# TESTING THE DASHBOARD PAGE WEEKLY CALCULATOR
+# GIVEN I have entered my monthly spending on rent as 1k on the form page
+def test_dashboard_weekly_calculator():
+# WHEN I click submit
+    test_list = [1000, 0, 0, 0, 0, 0, 0, 0, 0]
+    results = Finance('test').dashboard_weekly_calculator(test_list)
+# THEN my weekly rent spending should be calculated as £231
+    assert results == [231, 0, 0, 0, 0, 0, 0, 0, 0], "Dashboard Weekly Calculator Incorrect"
+
+
+
+# TESTING THE DASHBOARD ANNUAL CALCULATOR
+# GIVEN I have entered my monthly spending on rent as 1k on the form page
+@pytest.mark.parametrize("test_list", [
+        ([1000, 0, 0, 0, 0, 0, 0, 0, 0]),
+        ([-1000, 0, 0, 0, 0, 0, 0, 0, 0])
+    ])
+def test_dashboard_annual_calculator(test_list):
+# WHEN I click submit
+    results = Finance('test').dashboard_annual_calculator(test_list)
+# THEN my annual rent spending should be calculated as 12k
+    assert results[0] == 12000, f"Dashboard Annual Calculator Incorrect: {results[0]}"
+
+
+
+# TESTING THE BENEFITS TEMPLATE - a page where different text is selected from the database depending on which route was selected
+# GIVEN a selection of benefits to click on the nav bar
 def test_request_benefits(client):
+# WHEN I click on the Child Benefit option
     response = client.get("/child-benefit")
+# THEN I chould see a webpage that displays information about specifically child benefit
     assert b"You get Child Benefit if you are responsible for bringing up a child" in response.data
 
-# code to test simple debt calculator
+
+# GIVEN a simple debt calculator
 def test_simple_debt_calc():
+# WHEN I enter my debt amount of 10K, my interest rate of 5% and the loan period of 5 years 
     amount = 10000
     interest_rate = 5
     loan_term = 5
     results = Finance('test').simple_debt_calculator(amount, interest_rate, loan_term)
-    assert results == 12500, "Simple Debt Calculator has failed"
+# THEN the calculator should show me that the total cost of my loan over 12 years will be 12.5K
+    assert results == 12500, "Simple Debt Calculator Incorrect"
     
 
