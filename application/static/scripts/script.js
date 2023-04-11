419c8eea5ae7278be58320b76c59db31b5a85d1b
Ellen Houghton <ellen.a.ash@gmail.com>
2023-04-11 09:57:46 +0100

    add javascript disclaimer

---
+++ application/static/scripts/script.js
@@ -0,0 +1,6 @@
+
+
+function disclaimer() {
+    document.getElementById("disclaimer").innerHTML="DISCLAIMER: This website was designed by programming students and while useful, does not offer financial advice.";
+}
+
